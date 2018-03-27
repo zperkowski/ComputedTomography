fbe14c63f2fe03844b218ba6ac52294e5a202f33
Zdzisław Perkowski <ZdzislawPerkowski@outlook.com>
2018-03-27 23:23:47 +0200

    First window

--- src/com/zperkowski/Controller.java
+++ src/com/zperkowski/Controller.java
@@ -1,4 +1,46 @@
 package com.zperkowski;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Slider;
+
 public class Controller {
+
+    @FXML
+    Slider slider_rays;
+    @FXML
+    Slider slider_angle;
+    @FXML
+    Slider slider_rotation;
+    @FXML
+    Canvas canvas_center;
+    @FXML
+    Canvas canvas_right;
+
+    @FXML
+    public void generate() {
+        System.out.println("Rays: " + slider_rays.getValue() + " Angle: " + slider_angle.getValue() + " Rotation:" + slider_rotation.getValue());
+    }
+
+    @FXML
+    public void menu_new() {
+        slider_rays.setValue(1.0);
+        slider_angle.setValue(1.0);
+        slider_rotation.setValue(0.0);
+    }
+
+    @FXML
+    public void menu_open() {
+
+    }
+
+    @FXML
+    public void menu_save() {
+
+    }
+
+    @FXML
+    public void menu_about() {
+
+    }
 }
